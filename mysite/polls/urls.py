c647c21ea22537146d3beed955f9c3e7be45225d
Yauheni <busko-007@mail.ru>
2019-02-16 14:57:29 +0300

    first commit, start project

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path(r'', views.read, name='read'),
-    path(r'login/', views.login),
-    path(r'generation/', views.generation)
+    path(r'', views.read, name='read')
 ]
